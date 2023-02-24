8a43acbad6f1dbe0b61bc39931d1600b3f8da43e
U-MYCOMPUTER\Keishu <a5size.nalcoz@gmail.com>
2023-02-25 04:29:38 +0900

    Fine-tuned.

--- src/praparat.f90
+++ src/praparat.f90
@@ -530,52 +530,6 @@ contains
     end function dir_name
 
     !https://qiita.com/aisha/items/c41c09b0587ba6503733
-    !subroutine makedirs(outdir)
-    !    character(len=*), intent(in) :: outdir
-    !    character(len=256) command
-    !    write(command, *) 'if [ ! -d ', trim(outdir), ' ]; then mkdir -p ', trim(outdir), '; fi'
-    !    write(*, *) trim(command)
-    !    call system(command)
-    !end subroutine makedirs
-    !
-    !subroutine removedirs(outdir)
-    !    character(len=*), intent(in) :: outdir
-    !    character(len=256) command
-    !    write(command, *) 'if [ -d ', trim(outdir), ' ]; then rm -r ', trim(outdir), '; fi'
-    !    write(*, *) trim(command)
-    !    call system(command)
-    !end subroutine removedirs
-    !
-    !subroutine tardirs(outdir)
-    !    character(len=*), intent(in) :: outdir
-    !    character(len=256) command
-    !    write(command, *) 'if [ -d ', trim(outdir), ' ]; then tar -zcvf ', &
-    !         trim(adjustl(outdir))//'.tar.gz ', trim(outdir), '; fi'
-    !    write(*, *) trim(command)
-    !    call system(command)
-    !end subroutine tardirs
-    !
-    !subroutine removefile(outfile)
-    !    character(len=*), intent(in) :: outfile
-    !    character(len=256) command
-    !    write(command, *) 'if [ -f ', trim(outfile), ' ]; then rm ', trim(outfile), '; fi'
-    !    write(*, *) trim(command)
-    !    call system(command)
-    !end subroutine removefile
-    !
-    !subroutine tarfile(outfile)
-    !    character(len=*), intent(in) :: outfile
-    !    character(len=256) command
-    !    write(command, *) 'if [ -f ', trim(outfile), ' ]; then tar -zcvf ', &
-    !         trim(adjustl(outfile))//'.tar.gz ', trim(outfile), '; fi'
-    !    write(*, *) trim(command)
-    !    call system(command)
-    !end subroutine tarfile
-
-
-
-
-    !https://qiita.com/aisha/items/c41c09b0587ba6503733
     subroutine makedirs(outdir)
       character(len=*), intent(in) :: outdir
       character(len=256) command
@@ -644,14 +598,6 @@ contains
       write(*, *) trim(command)
       call system(command)
     end subroutine tarfile
-    
-    
-    
-
-
-
-
-
     
     subroutine sun_init(id)
         Integer :: id
