d3b9763d81788b627e9ca8592ee9708d5d86baeb
U-MYCOMPUTER\Keishu <a5size.nalcoz@gmail.com>
2023-03-08 22:45:57 +0900

    Fixed to work on Windows

--- utilities/export_video.py
+++ utilities/export_video.py
@@ -1,21 +1,28 @@
+import os
 import glob
 import sys
 import cv2
 
 
 def main():
+
+    if os.name == 'nt':
+        sep = "\\"
+    elif os.name == 'posix':
+        sep = "/"
+    
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
-    img = cv2.imread(glob.glob("./pic/*")[0])
+    img = cv2.imread(glob.glob(".{0}pic{0}*".format(sep))[0])
     height, width, channels = img.shape[:3]
 
-    video = cv2.VideoWriter('./video.mp4', fourcc, 60.0, (width, height))
+    video = cv2.VideoWriter('.{}video.mp4'.format(sep), fourcc, 60.0, (width, height))
     
     if not video.isOpened():
         print("Open failed.")
         exit(-1)
 
-    file_list = [ [int(fn.split("/")[-1].replace(".bmp", "")), fn] for fn in glob.glob("./pic/*") ]
+    file_list = [ [int(fn.split(sep)[-1].replace(".bmp", "")), fn] for fn in glob.glob(".{0}pic{0}*".format(sep)) ]
     file_list.sort()
 
     count = 0
