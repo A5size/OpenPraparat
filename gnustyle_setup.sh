a9da23424e72c3872621647a5b8dee25b06c6834
nodokaha <scpeee@wail.ch>
2023-03-11 09:43:16 +0900

    fix

--- gnustyle_setup.sh
+++ gnustyle_setup.sh
@@ -25,11 +25,11 @@ SOFTWARE.
 EOF
 autoscan
 sed -e "s/FULL-PACKAGE-NAME/OpenPraparat/" -i configure.scan
-sed -e "s/VERSION/0.1/" -i configure.scan
+sed -e "s/VERSION/0.0.1-beta/" -i configure.scan
 sed -e "13aAC_PROG_FC" -i configure.scan
 sed -e "15aAM_INIT_AUTOMAKE" -i configure.scan
 sed -e "17a# FIXME: Replace 'main' with a function in '-lquadmath':\nAC_CHECK_LIB([quadmath], [main])\n# FIXME: Replace 'main' with a function in '-lGL':\nAC_CHECK_LIB([GL], [main])\n# FIXME: Replace 'main' with a function in '-lGLU':\nAC_CHECK_LIB([GLU], [main])\n# FIXME: Replace 'main' with a function in '-lgdi32':\nAC_CHECK_LIB([gdi32], [main])\n# FIXME: Replace 'main' with a function in '-lgfortran':\nAC_CHECK_LIB([gfortran], [main])\n# FIXME: Replace 'main' with a function in '-lglfw':\nAC_CHECK_LIB([glfw], [main])\n# FIXME: Replace 'main' with a function in '-lglfw3':\nAC_CHECK_LIB([glfw3], [main])\n# FIXME: Replace 'main' with a function in '-limm32':\nAC_CHECK_LIB([imm32], [main])\n# FIXME: Replace 'main' with a function in '-lopengl32':\nAC_CHECK_LIB([opengl32], [main])\n# FIXME: Replace 'main' with a function in '-lpthread':\nAC_CHECK_LIB([pthread], [main])" -i configure.scan
-sed -e "s/src\/Makefile/Makefile src\/Makefile/" -i configure.scan
+#sed -e "s/src\/Makefile/Makefile src\/Makefile/" -i configure.scan
 mv configure.scan configure.ac
 echo SUBDIRS=src >> Makefile.am
 cat <<EOF > src/Makefile.am
