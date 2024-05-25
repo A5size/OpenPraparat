d660b8f7dde4dc9da9f4e0dfae9a4dd25615507f
A5size <a5size.nalcoz@gmail.com>
2024-05-26 05:12:27 +0900

    Changed export_video.py

--- utilities/export_video.py
+++ utilities/export_video.py
@@ -10,8 +10,15 @@ def main():
         sep = "\\"
     elif os.name == 'posix':
         sep = "/"
-    
+
+    args = sys.argv
+    if len(args)!=1:
+        every = int(args[1])
+    else:
+        every = 1
+        
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    #fourcc = cv2.VideoWriter_fourcc(*'x264')
 
     img = cv2.imread(glob.glob(".{0}pic{0}*".format(sep))[0])
     height, width, channels = img.shape[:3]
@@ -24,7 +31,7 @@ def main():
 
     file_list = [ [int(fn.split(sep)[-1].replace(".bmp", "")), fn] for fn in glob.glob(".{0}pic{0}*".format(sep)) ]
     file_list.sort()
-
+    
     count = 0
     for num, file_name in file_list:
         
@@ -33,10 +40,13 @@ def main():
             print("{} cannot be read.".format(file_name))
             break
 
-        video.write(img)
         count += 1
-        
-        print(count, num)
+
+        if (count-1)%every==0:
+            video.write(img)
+            print(count, num, True)
+        else:
+            print(count, num, False)
         
     video.release()
     print('Exporting is complete.')
